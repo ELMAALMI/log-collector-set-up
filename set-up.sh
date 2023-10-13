45e85be2ef0fe6dc33799dbfedc27aa1c87b9450
ELMAALMI <tcsbilal@gmail.com>
2023-10-13 23:21:05 +0100

    last commit

--- set-up.sh
+++ set-up.sh
@@ -9,28 +9,32 @@ if java -version 2>&1 | grep -q "version \"$java_version"; then
 else
     echo "Java $java_version is not installed, installing now..."
     sudo apt update
-    sudo apt install -y openjdk-$java_version-jdk
+    sudo apt install openjdk-$java_version-jdk
 fi
 
 
 
 # install logger script
+
 useradd -aG adm logger
 
-mkdir -r /opt/logger
-mkdir -r /opt/logger/config
+mkdir -p /opt/logger/config
 
 cd /opt/logger
 
 wget -c https://raw.githubusercontent.com/ELMAALMI/log-collector-set-up/main/logger.service
+wget -c https://raw.githubusercontent.com/ELMAALMI/log-collector-set-up/main/application.properties
 wget -c https://github.com/ELMAALMI/log-collector-set-up/blob/main/log-collector.jar
+wget -c https://raw.githubusercontent.com/ELMAALMI/log-collector-set-up/main/log_config.txt
+
 
 chmod 777 /opt/logger
 
-cp logger/script /opt/logger
-
 # create linux service to run the script on the backround
-cp logger/logger.service /etc/systemd/system/
+cp logger.service /etc/systemd/system/
+cp application.properties /opt/logger/config
+cp log_config.txt /opt/logger/config
+
 
 systemctl daemon-reload
 systemctl enable logger.service
